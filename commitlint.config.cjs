fa2fc6231540baadc089fa2e2ce2693fb913c583
隔壁老蔡 <15918965226@139.com>
2024-09-07 16:31:33 +0800

    build: :package: 新增commitizen、cz-git、czg插件，规范化代码提交git流程和内容

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -4,6 +4,7 @@ module.exports = {
   rules: {
     // @see: https://commitlint.js.org/#/reference-rules
   },
+  extends: ['@commitlint/config-conventional'],
   prompt: {
     alias: { fd: 'docs: fix typos' },
     messages: {
